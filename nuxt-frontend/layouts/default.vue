0db392577eca2a309781e17cb7d9307e9c875a08
Alienbushman <bla31425@gmail.com>
2025-04-26 19:11:17 +0200

    Updated navigation to prevent mounting hydration problems and to adjust to the global theming

--- nuxt-frontend/layouts/default.vue
+++ nuxt-frontend/layouts/default.vue
@@ -3,6 +3,13 @@ import {useTheme} from "vuetify";
 import AppNavigation from "~/layouts/components/AppNavigation.vue";
 import {navigationItems} from "~/layouts/constants/navigation";
 
+// mounting first to prevent hydration problems
+const mounted = ref(false);
+
+onMounted(() => {
+  mounted.value = true;
+});
+
 const drawer = ref(true)
 
 // setting dark mode
@@ -12,53 +19,54 @@ const isLightTheme = ref(theme.global.name.value === 'light');
 
 watch(isLightTheme, (newValue: boolean) => {
 
-	const themeName = newValue ? 'light' : 'dark';
+  const themeName = newValue ? 'light' : 'dark';
 
-	theme.global.name.value = themeName;
+  theme.global.name.value = themeName;
 });
 
 
 </script>
 <template>
-	<v-app>
-		<v-app-bar
-				:elevation="2"
-				density="comfortable">
-			<v-app-bar-nav-icon
-					@click.stop="drawer = !drawer"
-			/>
-			<v-toolbar-title title="Bootstrap project">
-				<span class="font-weight-bold">Bootstrapped project </span>
-			</v-toolbar-title>
-			<v-switch
-					class="pt-5 pr-3 "
-					v-model="isLightTheme"
-					color='primary'
-					true-icon='mdi-weather-sunny'
-					false-icon='mdi-weather-night'
-					inset
-			/>
-		</v-app-bar>
+  <v-app>
+    <v-app-bar class="bg-background"
+               :elevation="2"
+               density="comfortable">
+      <v-app-bar-nav-icon
+          @click.stop="drawer = !drawer"
+      />
+      <v-toolbar-title title="Bootstrap project">
+        <span class="font-weight-bold">Bootstrapped project </span>
+      </v-toolbar-title>
+      <v-switch
+          class="pt-5 pr-3 "
+          v-model="isLightTheme"
+          color='primary'
+          true-icon='mdi-weather-sunny'
+          false-icon='mdi-weather-night'
+          inset
+      />
+    </v-app-bar>
 
-		<!--Add the following to get it to expand on hover-->
-		<!--				v-model="drawer"-->
-		<!--				expand-on-hover-->
-		<v-navigation-drawer
-				:rail="drawer"
-				:rail-width="70"
-				:width="280"
-				permanent
-		>
-			<AppNavigation :items="navigationItems"/>
+    <!--Add the following to get it to expand on hover-->
+    <!--				v-model="drawer"-->
+    <!--				expand-on-hover-->
+    <v-navigation-drawer class="bg-background"
+                         v-if="mounted"
+                         :rail="drawer"
+                         :rail-width="70"
+                         :width="280"
+                         permanent
+    >
+      <AppNavigation :items="navigationItems"/>
 
-		</v-navigation-drawer>
+    </v-navigation-drawer>
 
-		<v-main>
-			<v-container>
-				<slot/>
-			</v-container>
-		</v-main>
-	</v-app>
+    <v-main>
+      <v-container>
+        <slot/>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <style scoped>
